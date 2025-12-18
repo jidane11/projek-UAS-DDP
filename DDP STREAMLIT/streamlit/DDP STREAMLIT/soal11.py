3382921b261a8549f9eba1b05bbbb27bfaba9e2e
jidane11 <164849806+jidane11@users.noreply.github.com>
2025-12-18 18:23:12 +0700

    Delete DDP STREAMLIT/streamlit directory

--- DDP STREAMLIT/streamlit/DDP STREAMLIT/soal11.py
+++
@@ -1,8 +0,0 @@
-kendaraan = ["namaKendaraan", "jenisKendaraan","ccKendaraan","warnaKendaraan","rodaKendaraan" ]
-
-
-kendaraan.extend (["hargaKendaraan", "tipeKendaraan"])
-
-kendaraan.insert (2, "merkKendaraan")
-
-print(kendaraan)
